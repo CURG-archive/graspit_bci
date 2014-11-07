7ff051201d2b9d2c559333b453f8d876c84a178c
Jon Weisz <jweisz@cs.columbia.edu>
2014-11-06 21:41:36 -0500

    Added arrows to the guides so that the use can see which direction they turn in.

--- src/BCI/utils/uiTools.cpp
+++ src/BCI/utils/uiTools.cpp
@@ -10,6 +10,7 @@
 #include "BCI/utils/worldElementTools.h"
 #include "SoRing.h"
 #include <Inventor/SbColor.h>
+#include "SoArrow.h"
 
 using bci_experiment::world_element_tools::getWorld;
 using bci_experiment::world_element_tools::getObjectByName;
@@ -92,14 +93,95 @@ namespace bci_experiment
           return true;
         }
 
+        int numArrows = 5;
+        float arrowRadius = 15;
+
+        SoSeparator * createAxisArrow(SoSeparator * axisRoot, unsigned int arrowInd, SbColor & color)
+        {
+            SoSeparator * arrowSeparator = new SoSeparator(3);
+            SbString axisName(axisRoot->getName().getString());
+            SbString arrowName(axisName +  "_ArrowRoot"  + QString::number(arrowInd).toStdString().c_str());
+            arrowSeparator->setName(arrowName);
+            axisRoot->addChild(arrowSeparator);
+
+            SoTransform * arrowTran = new SoTransform();
+            arrowTran->setName(arrowName + "_Tran");
+            SoMaterial * arrowMat = new SoMaterial();
+            arrowMat->setName(arrowName + "_Mat");
+            SoArrow * arrowRoot = new SoArrow();
+            arrowRoot->setName(arrowName + "_GeomRoot");
+
+            arrowSeparator->addChild(arrowTran);
+            arrowSeparator->addChild(arrowMat);
+            arrowSeparator->addChild(arrowRoot);
+
+            //arrowTran->rotation.setValue(SbVec3f(1,0,0), (float)(M_PI/2.0));
+            arrowMat->transparency = 0.0;
+            arrowMat->ambientColor.setValue(color);
+            arrowMat->emissiveColor.setValue(0.2,0.2,0.2);
+            arrowMat->diffuseColor.setValue(color);
+
+            arrowRoot->cylRadius = 5;
+            arrowRoot->coneRadius = arrowRadius;
+            arrowRoot->height = 50;
+            arrowRoot->coneHeight = 50;
+
+        }
+
+
+        SoSeparator * createAxisArrowGroup(SoSeparator * axisRoot, SbColor & color)
+        {
+            SbString axisName(axisRoot->getName().getString());
+            SbString groupName(axisName + "_ArrowGroup");
+            SoSeparator * arrowGroup = new SoSeparator(numArrows);
+            arrowGroup->setName(groupName);
+            axisRoot->addChild(arrowGroup);
+            for(int i = 0; i < numArrows; ++i)
+            {
+                createAxisArrow(arrowGroup, i, color);
+            }
+        }
+
+
+        void updateAxisArrow(SoSeparator * axisRoot, float theta, float radius, unsigned int arrowInd)
+        {
+            SbString axisName(axisRoot->getName().getString());
+            SbString arrowName(axisName + "_ArrowRoot" + QString::number(arrowInd).toStdString().c_str());
+            SbString tranName(arrowName + "_Tran");
+            SoSeparator * arrowSep = static_cast<SoSeparator *>(axisRoot->getByName(arrowName));
+            SoTransform * arrowTran = static_cast<SoTransform *>
+                    (arrowSep->getByName(tranName));
+            radius = radius - arrowRadius/2;
+            vec3 position = vec3::ZERO;
+            position[0] = radius*sin(theta);
+            position[1] = radius*cos(theta);
+            arrowTran->translation.setValue(position.toSbVec3f());
+            arrowTran->rotation.setValue(SbVec3f(0,0,1), M_PI/2-theta);
+        }
+
+
+        void updateAxisArrowGroup(SoSeparator * axisRoot, float radius)
+        {
+            SbString axisName(axisRoot->getName().getString());
+            SbString groupName(axisName + "_ArrowGroup");
+            SoSeparator * arrowGroup = static_cast<SoSeparator *>(axisRoot->getByName(groupName));
+            for(int i = 0; i < numArrows; ++i)
+            {
+                updateAxisArrow(arrowGroup, i*2*M_PI/numArrows, radius, i);
+            }
+
+        }
+
+
+
         // Create a new node set representing an axis grounded at an object
         SoSeparator * createNewGuideAxis(std::string axisName, const vec3 & ringEuler, SbColor & color)
         {
-            SoSeparator * axisSep = new SoSeparator(3);
+            SoSeparator * axisSep = new SoSeparator();
             SoMaterial * axisMat = new SoMaterial();
             axisMat->transparency = 0.0;
             axisMat->ambientColor.setValue(color);
-            axisMat->emissiveColor.setValue(color);
+            axisMat->emissiveColor.setValue(.2,.2,.2);
             axisMat->diffuseColor.setValue(color);
             SoTransform * axisTran = new SoTransform();
             SoRing * axisGeom = new SoRing();
@@ -113,6 +195,7 @@ namespace bci_experiment
             axisSep->addChild(axisTran);
             axisSep->addChild(axisGeom);
             rotXYZ(ringEuler[0], ringEuler[1], ringEuler[2]).toSoTransform(axisTran);
+            createAxisArrowGroup(axisSep, color);
             return axisSep;
         }
 
@@ -122,6 +205,7 @@ namespace bci_experiment
         {
             static_cast<SoRing *>(ringSep->getChild(2))->outerRadius = radius;
             static_cast<SoRing *>(ringSep->getChild(2))->innerRadius = radius - thickness;
+            updateAxisArrowGroup(ringSep, radius);
         }
 
         void setRingTransform(SoSeparator * ringSep,
@@ -150,7 +234,7 @@ namespace bci_experiment
             SbColor blue;
             blue.setValue(.5,.5,1);
             SoSeparator * xAxisSep =  createNewGuideAxis("XAxisGuide", vec3(0,M_PI/2,0), pink);
-            SoSeparator * zAxisSep = createNewGuideAxis("ZAxisGuide", vec3(0,0,0), blue);
+            SoSeparator * zAxisSep = createNewGuideAxis("ZAxisGuide", vec3(0,0,0), blue);            
 
             //Add children to root
             guideSeparator->addChild(objectTransform);
@@ -162,6 +246,7 @@ namespace bci_experiment
             return guideSeparator;
 
         }
+
 
 
         void updateCircularGuides(Hand *hand, Body *object)
@@ -209,7 +294,7 @@ namespace bci_experiment
 
             //Set Z ring offset
             setRingTransform(xAxisSep, handToObjectTran, 0);
-            setRingTransform(zAxisSep, handToObjectTran, 2);
+            setRingTransform(zAxisSep, handToObjectTran, 2);                        
         }
 
         void destroyGuideSeparator()
