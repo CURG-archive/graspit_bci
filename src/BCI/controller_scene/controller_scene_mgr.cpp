059b1ee9473e6a80ff007af0f72b2c593477c913
jvarley <jakevarley@gmail.com>
2015-10-27 20:31:50 -0400

    fixed sprite collisions.

--- src/BCI/controller_scene/controller_scene_mgr.cpp
+++ src/BCI/controller_scene/controller_scene_mgr.cpp
@@ -5,13 +5,19 @@
 #include "debug.h"
 #include "Inventor/nodes/SoAnnotation.h"
 #include <Inventor/SoDB.h>
+#include "BCI/bciService.h"
+
+
 
 ControllerSceneManager::ControllerSceneManager(SoAnnotation *control_scene_separator_)
     : control_scene_separator(control_scene_separator_),
       state(CursorState::SPINNING)
 {
 
-    cursor = new Cursor(control_scene_separator, QString("sprites/cursor_scaled.png"), -1.0, 0.0, 0.0);
+    //cursor = new Cursor(control_scene_separator, QString("sprites/cursor_scaled.png"), -0.05, 0.0, 0.0);
+    cursor = new Cursor(control_scene_separator, QString("sprites/cursor_scaled.png"), -.8, -1.0, 0.0);
+    //cursor = new Cursor(control_scene_separator, QString("sprites/cursor_scaled.png"), -0.1, 0.05, 0.0);
+
 }
 
 void ControllerSceneManager::addTarget(Target *t)
@@ -28,14 +34,22 @@ void ControllerSceneManager::clearTargets()
         SoDB::writeunlock();
     }
     targets.clear();
+//    Target *t  = new Target(control_scene_separator, QString("sprites/cursor_scaled.png"), 0.05, 0.0, 0.0);
+//    Target *t2  = new Target(control_scene_separator, QString("sprites/cursor_scaled.png"), 0.025, 0.025, 0.0);
+//    targets.push_back(t);
+//    targets.push_back(t2);
 }
 
 void ControllerSceneManager::update()
 {
-    cursor->update(state);
+    float pixels_per_inch = BCIService::getInstance()->bciRenderArea->getViewportRegion().getPixelsPerInch();
+    short renderAreaWidth = BCIService::getInstance()->bciRenderArea->getSize()[0];
+    short renderAreaHeight = BCIService::getInstance()->bciRenderArea->getSize()[1];
+
+    cursor->update(state, pixels_per_inch, renderAreaWidth, renderAreaHeight);
     for(int i = 0; i < targets.size(); i++)
     {
-        targets[i]->update(state);
+        targets[i]->update(state, pixels_per_inch, renderAreaWidth, renderAreaHeight);
         if (targets[i]->intersects(cursor->bounding_rect))
         {
             targets[i]->setHit();
--- src/BCI/controller_scene/sprites.cpp
+++ src/BCI/controller_scene/sprites.cpp
@@ -5,12 +5,12 @@
 #include <Inventor/nodes/SoAnnotation.h>
 #include <Inventor/nodes/SoTransform.h>
 #include <Inventor/nodes/SoMaterial.h>
+#include "BCI/bciService.h"
 
 #include <iostream>
 
 #define PI 3.14159265
 
-#define PIXEL_TO_UNIT 0.00348
 #define MIN_X  -1.4
 #define MAX_X  1.25
 #define MIN_Y  -1.015
@@ -22,7 +22,7 @@ Cursor::Cursor(SoAnnotation * control_scene_separator, QString filename, double 
 
 }
 
-void Cursor::update(int state)
+void Cursor::update(int state, float pixels_per_inch, short renderAreaWidth, short renderAreaHeight)
 {
     if(state == CursorState::MOVING_SLOW)
     {
@@ -63,16 +63,34 @@ void Cursor::update(int state)
     QImage rotated_img;
     QTransform rotating;
     rotating.rotate(theta);
-    int height = qimage->height();
-    int width = qimage->width();
-    bounding_rect = new QRectF(x,y, height*PIXEL_TO_UNIT, width*PIXEL_TO_UNIT);
-    rotated_img = qimage->transformed(rotating);
+    int orig_height = qimage->height();
+    int orig_width = qimage->width();
+
+    //rotated_img = rotated_img.scaled(width/renderAreaWidth, height/ renderAreaHeight, Qt::KeepAspectRatio);
+
+
+    std::cout << "Pixels Per Inch" << std::endl;
+    std::cout << pixels_per_inch << std::endl;
+    std::cout << "Render Width" << std::endl;
+    std::cout << renderAreaWidth << std::endl;
+    std::cout << "Render Height" << std::endl;
+    std::cout << renderAreaHeight << std::endl;
+
+    QImage scaled_img;
+    scaled_img = qimage->scaled(int(1.0/20 *renderAreaHeight), int(1.0/20*renderAreaWidth), Qt::KeepAspectRatio);
+    rotated_img = scaled_img.transformed(rotating);
+
+    int height = scaled_img.height();
+    int width = scaled_img.width();
 
     convert(rotated_img.copy(QRect((rotated_img.width()-width)/2,
                                    (rotated_img.height()-height)/2,
                                    width,
                                    height)), image->image);
 
+    bounding_rect = new QRectF(x,y,  orig_height/500.0, orig_width/500.0);
+    //bounding_rect = new QRectF(x,y,  height/500.0, width/500.0);
+    //bounding_rect = new QRectF(x,y,  height/(1.0*orig_height)*orig_height*0.00348, width/(1.0*orig_width)*orig_width*0.00348);
     imageTran->translation.setValue(SbVec3f(x,y,0));
 
 //    std::cout << "x: ";
@@ -97,7 +115,7 @@ void Target::setHit()
     }
 }
 
-void Target::update(int state)
+void Target::update(int state, float pixels_per_inch, short renderAreaWidth, short renderAreaHeight)
 {
     if(steps_since_last_hit == 5)
     {
@@ -109,6 +127,25 @@ void Target::update(int state)
         steps_since_last_hit += 1;
         image->filename = "sprites/target_hit.png";
     }
+    int orig_height = qimage->height();
+    int orig_width = qimage->width();
+
+    QImage scaled_img;
+    scaled_img = qimage->scaled(int(1.0/10 *renderAreaHeight), int(1.0/10*renderAreaWidth), Qt::KeepAspectRatio);
+
+    int height = scaled_img.height();
+    int width = scaled_img.width();
+
+    std::cout << orig_width << std::endl;
+    std::cout << width << std::endl;
+    convert(scaled_img.copy(QRect((scaled_img.width()-width)/2,
+                                   (scaled_img.height()-height)/2,
+                                   width,
+                                   height)), image->image);
+
+
+    //bounding_rect = new QRectF(x,y,  height/500.0, width/500.0);
+    bounding_rect = new QRectF(x,y,  orig_height/500.0, orig_width/500.0);
 //    else if(steps_since_last_hit == 6)
 //    {
 //        image->filename = filename.toStdString().c_str();
@@ -135,7 +172,9 @@ Sprite::Sprite(SoAnnotation *control_scene_separator,
 
     convert(image->image, *qimage);
 
-    bounding_rect = new QRectF(x,y,qimage->height()*PIXEL_TO_UNIT, qimage->width()*PIXEL_TO_UNIT);
+    float pixels_per_inch = 0.1;// BCIService::getInstance()->bciRenderArea->getViewportRegion().getPixelsPerInch();
+
+    bounding_rect = new QRectF(x,y,qimage->height()/pixels_per_inch, qimage->width()/pixels_per_inch);
 
     imageTran = new SoTransform;
     imageTran->translation.setValue(SbVec3f(x,y,0));
@@ -171,34 +210,44 @@ bool Sprite::intersects(QRectF *other_rect)
 
     double dist = sqrt(pow(my_center_x-other_center_x,2) + pow(my_center_y-other_center_y,2));
 
-//    std::cout << "Checking intersections" << std::endl;
-//    std::cout << "RECT 1" << std::endl;
-//    std::cout << other_rect->x();
-//    std::cout << " ";
-//    std::cout << other_rect->y();
-//    std::cout << " ";
-//    std::cout << other_rect->height();
-//    std::cout << " ";
-//    std::cout << other_rect->width() <<std::endl;
+    std::cout << "Checking intersections" << std::endl;
+    std::cout << "RECT 1" << std::endl;
+    std::cout << "x: ";
+    std::cout << other_rect->x();
+    std::cout << " y: ";
+    std::cout << other_rect->y();
+    std::cout << " h: ";
+    std::cout << other_rect->height();
+    std::cout << " w: ";
+    std::cout << other_rect->width();
+    std::cout << " center_x: ";
+    std::cout << other_center_x;
+    std::cout << " center_y: ";
+    std::cout << other_center_y<<std::endl;
 
-//    std::cout << "RECT 2" << std::endl;
-//    std::cout << bounding_rect->x();
-//    std::cout << " ";
-//    std::cout << bounding_rect->y();
-//    std::cout << " ";
-//    std::cout << bounding_rect->height();
-//    std::cout << " ";
-//    std::cout << bounding_rect->width() <<std::endl;
+    std::cout << "RECT 2" << std::endl;
+    std::cout << "x: ";
+    std::cout << bounding_rect->x();
+    std::cout << " y: ";
+    std::cout << bounding_rect->y();
+    std::cout << " h: ";
+    std::cout << bounding_rect->height();
+    std::cout << " w:";
+    std::cout << bounding_rect->width();
+    std::cout << " center_x: ";
+    std::cout << my_center_x;
+    std::cout << " center_y: ";
+    std::cout << my_center_y<<std::endl;
 
     std::cout << "Distance Between Target and Cursor: ";
     std::cout << dist;
 
-    double min_dist_for_collision = .261;
-    //double min_dist_for_collision = other_radius + my_radius;
-
+    //double min_dist_for_collision = .261;//.076
+    double min_dist_for_collision = other_radius + my_radius;
+    //double min_dist_for_collision = .0002* BCIService::getInstance()->bciRenderArea->getSize()[0];
     std::cout << "Min Distance For collision: ";
     std::cout << min_dist_for_collision << std::endl;
-
+    //return this->bounding_rect->intersects(*other_rect);
     //return (other_radius + my_radius) > dist;
     if ((min_dist_for_collision) > dist)
     {
